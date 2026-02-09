478c0aefe45c54f79adee958786eff43f693c75c
AVilgats <annabel.vilgats@tktk.ee>
2026-02-09 19:05:41 +0200

    Update water_intake_calculator.py

---
+++ water_intake_calculator.py
@@ -0,0 +1,12 @@
+#Veejoomise kalkulaator
+goal = 2 liitrit
+water = int(input("Mitu liitrit vett Te juba joonud olete?:"))
+percent = (water/goal) * 100
+print(f"{percent}%")
+if percent < 75:
+    print("Joo rohkem vett, keha vajab seda!")
+elif print < 100:
+    print("Tubli, jätka samas vaimus!")
+else:
+    print("Suurepärane, oled oma päevase eesmärgi täitnud!")
+    
